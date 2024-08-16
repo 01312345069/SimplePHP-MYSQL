f071b30415eb8a6c238178215c62f2071972b795
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2024-08-17 01:33:07 +0600

    3nd commit

--- index.php
+++ index.php
@@ -8,8 +8,19 @@
 </head>
 <body>
     <div class="container">
-        <h3>Welcome to my WEBSITE</h3>
+        <h3>Welcome to WEBSITE</h3>
         <p>Enter your detail to buy ticket</p>
+
+        <form action="index.php" method="post"></form>
+        <input type="text" name="Name" id="Name" placeholder="Enter your Name">
+        <input type="text" name="Age" id="Age" placeholder="Enter your Age">
+        <input type="text" name="Number" id="Number" placeholder="Enter your Number">
+        <input type="text" name="Email" id="Email" placeholder="Enter your Email">
+        <input type="text" name="Gender" id="Gender" placeholder="Enter your Gender">
+        
+        <textarea name="Detail" id="Detail" placeholder="Enter your detail"></textarea>
     </div>
+
+    <script src="index.js"></script>
 </body>
 </html>
